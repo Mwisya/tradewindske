68fd86d5834defe5c1e73ec5b706571d363cd13f
Mwisya Julius <fm@gmail.com>
2022-05-09 10:35:20 +0300

    adjustmenst

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className='container'>
             <article>
 
-            We are software developments who specialise in website development.we provide day to day solutions to clients who want to have space in the global market. This means a business is able to be accessible from anywhere. We offer unique and well designed websites  with outstanding UI/UX design to suit your business needs.
+            We are software developers who specialise in website development.we provide day to day solutions to clients who want to have space in the global market. This means a business is able to be accessible from anywhere. We offer unique and well designed websites  with outstanding UI/UX design to suit your business needs.
             <br/>
             We ensure that your physical shop can be accessed by anyone who require your service. We build custom made websites to not only cross borders but also expand and create new market for your business and guarantee returns. We provide modern ICT solutions to enable customer to business interaction and vice versa.
             <br/>
